bc9ebdcca0c315d8abf86067936a7eba6bb73d2d
niraj-khatiwada <nirazzz.nk@gmail.com>
2024-11-24 21:26:31 +0545

    fix: add deploy script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-# cd /home/niraj/Documents/nestjs-fastify-boilerplate
-# git reset --hard HEAD
-# git pull origin main
-# docker build --tag nestjs-boilerplate-prod:latest . --no-cache
-# pnpm docker:prod:down
-# pnpm docker:prod:up
-# docker volume prune -f
